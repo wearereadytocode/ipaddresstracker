24c6fd88f37ea6c2f3f925acba4f36ab4889e43b
vijayskk <vijayskk007@gmail.com>
2021-07-13 15:22:19 +0530

    ui and api catching ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Home from './components/Home';
+import { DetailsContextProvider } from './contexts/DetailsContext';
+import { ViewPortContextProvider } from './contexts/ViewPortContext';
 
 function App() {
   return (
     <div className="App">
-      
+      <ViewPortContextProvider>
+      <DetailsContextProvider>
+        <Home />
+      </DetailsContextProvider>
+      </ViewPortContextProvider>
     </div>
   );
 }
